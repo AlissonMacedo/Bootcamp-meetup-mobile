54dc643b60556772c64c290383d3fef2c62c6f39
Alisson <alissontecnico@gmail.com>
2019-08-29 20:19:06 -0300

    creating components

---
+++ src/components/DatePicker/index.android.js
@@ -0,0 +1,34 @@
+import React, {useMemo} from 'react';
+import {DatePickerAndroid} from 'react-native';
+import {format} from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import {Container, DateButton, DateText} from './styles';
+
+export default function DatePicker({date, onChange}) {
+  const dateFormatted = useMemo(
+    () => format(date, "dd 'de' MMMM", {locale: pt}),
+    [date],
+  );
+
+  async function handleOpenPicker() {
+    const {action, year, month, day} = await DatePickerAndroid.open({
+      mode: 'spinner',
+      date,
+    });
+
+    if (action === DatePickerAndroid.dateSetAction) {
+      const selectedDate = new Date(year, month, day);
+
+      onChange(selectedDate);
+    }
+  }
+
+  return (
+    <Container>
+      <DateButton onPress={handleOpenPicker}>
+        <DateText>{dateFormatted}</DateText>
+      </DateButton>
+    </Container>
+  );
+}
