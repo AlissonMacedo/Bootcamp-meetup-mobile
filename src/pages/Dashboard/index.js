b95226bba8993d51b6e90d9d7146c153961f3cba
Alisson <alissontecnico@gmail.com>
2019-08-30 00:09:41 -0300

    add funtion login and signup pages

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import {View} from 'react-native';
-
-// import { Container } from './styles';
+import {Text} from 'react-native';
 
 export default function Dashboard() {
-  return <View />;
+  return <Text>Alisson</Text>;
 }
