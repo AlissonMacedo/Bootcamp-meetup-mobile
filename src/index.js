66bc56c9f117e43012e1cca492939303efa3010c
Alisson <alissontecnico@gmail.com>
2019-08-29 21:29:21 -0300

    config routes and app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
+import {StatusBar} from 'react-native';
 
-import '~/config/ReactotronConfig';
+import './config/ReactotronConfig';
 
-import Routes from '~/routes';
+import {store, persistor} from './store';
 
-const App = () => <Routes />;
+import App from '../App';
 
-export default App;
+export default function Index() {
+  return (
+    <>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#121212" />
+          <App />
+        </PersistGate>
+      </Provider>
+    </>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,44 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation';
 
-import Main from '~/pages/Main';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
-const Routes = createAppContainer(createSwitchNavigator({ Main }));
+import Dashboard from './pages/Dashboard';
+import Subscription from './pages/Subscription';
+import Profile from './pages/Profile';
 
-export default Routes;
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Subscription,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#2B1A2F',
+              },
+            },
+          },
+        ),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      },
+    ),
+  );
